f059474489714757cec0fcb8be2dc36a3091a5a2
Kory Smith <kor54e@gmail.com>
2024-10-09 16:18:18 -0400

    adds design colors to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      "dark-orange": "#C65842",
+			"orange": "#FF931E",
+			"purple": "#330333",
+			"black": "#140314",
+			"white": "#FFFFFF",
+			"magenta": "#8E2E50",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
