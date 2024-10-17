7725332f5fcefc1b3ab72409a433f48d31d13448
Kory Smith <kor54e@gmail.com>
2024-10-17 09:45:16 -0400

    sets up base styles and fonts and such

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,13 +4,36 @@ export default {
   theme: {
     colors: {
       "dark-orange": "#C65842",
-			"orange": "#FF931E",
-			"purple": "#330333",
-			"black": "#140314",
-			"white": "#FFFFFF",
-			"magenta": "#8E2E50",
+      orange: "#FF931E",
+      purple: "#330333",
+      black: "#140314",
+      white: "#FFFFFF",
+      magenta: "#8E2E50",
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        // https://github.com/system-fonts/modern-font-stacks?tab=readme-ov-file#monospace-code
+        header: [
+          "Inter",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial Nova",
+          "Nimbus Sans",
+          "Arial",
+          "sans-serif",
+        ],
+        // https://github.com/system-fonts/modern-font-stacks?tab=readme-ov-file#monospace-code
+        body: [
+          "ui-monospace",
+          "Cascadia Code",
+          "Source Code Pro",
+          "Menlo",
+          "Consolas",
+          "DejaVu Sans Mono",
+          "monospace",
+        ],
+      },
+    },
   },
   plugins: [],
 };
