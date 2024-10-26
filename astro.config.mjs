bc19e684b2a5e594033faac751ac3a332937df78
Kory Smith <kor54e@gmail.com>
2024-10-26 17:19:25 -0400

    adds cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false })],
+  output: "server",
+  adapter: cloudflare(),
 });
